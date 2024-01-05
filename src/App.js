9f765ef968b739f76c7bdf615a04f706502c1a5e
Jarrod Bogard <jarrodbogard@gmail.com>
2024-01-05 09:19:18 -0500

    -initial commit - fetch data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState([]);
+  const [isloading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const response = await fetch(`https://swapi.dev/api/people/`);
+
+      const fetchedData = await response.json();
+
+      setData(fetchedData.results);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(data);
+
+  return <div className="App"></div>;
 }
 
 export default App;
